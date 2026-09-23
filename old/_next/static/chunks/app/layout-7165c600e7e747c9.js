e05793bd7d34125f48c9ef9a167dec51fa9005fe
gustafomkl-rgb <gustafomkl@gmail.com>
2026-09-23 05:08:09 +0300

    Add /old/ preview: previous gold/violet SoJournalo design

---
+++ old/_next/static/chunks/app/layout-7165c600e7e747c9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{2494:function(n,e,u){Promise.resolve().then(u.t.bind(u,1025,23))},1025:function(){}},function(n){n.O(0,[437,281,914,744],function(){return n(n.s=2494)}),_N_E=n.O()}]);
---
+++ old/_next/static/pocwXlpKZqUt4VrXr3vVZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-5c26db8b43959318.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
